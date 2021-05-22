28e3332e0787970d118d17a25c57fd26924805c0
lDanielZasso <83052163+lDanielZasso@users.noreply.github.com>
2021-05-22 13:19:18 -0400

    Nuevas modificaciones

--- src/Ejercicios/Ejercicio10.java
+++ src/Ejercicios/Ejercicio10.java
@@ -6,10 +6,12 @@ import java.awt.Graphics;
 
 public class Ejercicio10 extends Applet {
 
+    @Override
     public void init() {
         
     }
     
+    @Override
     public void paint(Graphics g){
         
     }
