eb9a800cc8debae81b5fd0d381d326cf235c35f7
danie <danie@DESKTOP-I8EKCGE>
2021-05-26 23:06:34 -0400

    Última modificación

--- src/Ejercicios/Ejercicio10.java
+++ src/Ejercicios/Ejercicio10.java
@@ -8,12 +8,35 @@ public class Ejercicio10 extends Applet {
 
     @Override
     public void init() {
-        
+        resize(600, 600);
     }
     
     @Override
     public void paint(Graphics g){
+        int y = 190, x;
+        g.drawRect(200, 200, 200, 150);
         
+        for(x=200; x<=395; x++){
+            g.fillRect(x, y, 10, 10);
+            pausa(10);
+            g.clearRect(x, y, 10, 10);
+        }
+        
+        for(y=190; y<=345; y++){
+            g.fillRect(x, y, 10, 10);
+            pausa(10);
+            g.clearRect(x, y, 10, 10);
+            g.drawRect(200, 200, 200, 150);
+        }
+    
     }
+    
+    public void pausa(int n){
+            try{
+                Thread.sleep(n);
+            }catch(InterruptedException e){
+                
+            }
+        }
 
 }
